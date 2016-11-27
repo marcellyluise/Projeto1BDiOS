31f9568991aa1001e769e8d75628f6275c6e9dc9
Marcelly Luise <marcellygodinho@gmail.com>
2016-11-27 18:26:37 -0200

    Begin creating Model

---
+++ BolsaFamilia Analytics/Favored.swift
@@ -0,0 +1,14 @@
+//
+//  Favored.swift
+//  BolsaFamilia Analytics
+//
+//  Created by Marcelly Luise Souza Godinho on 27/11/16.
+//  Copyright © 2016 UnB. All rights reserved.
+//
+
+import UIKit
+import EVReflection
+
+class Favored: EVObject {
+
+}
